cb163dd7e8e76e329115a6dd481bd1a6e60b6e00
Alex Li <alexyli0330@gmail.com>
2018-12-17 18:22:37 +0800

    Bug fixed

--- src/com/painter/Painter.java
+++ src/com/painter/Painter.java
@@ -1,8 +1,17 @@
 package com.painter;
 
+import com.board.Board;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Painter {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+	}
+
+	public static void DrawBoard(Graphics graphics, Board board) {
+
 	}
 }
