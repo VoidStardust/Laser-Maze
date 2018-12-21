a2f72ec87d47e5ae5913cc0ad79d1465e2d1a4bc
Alex Li <alexyli0330@gmail.com>
2018-12-21 19:29:51 +0800

    Bug fixed

---
+++ src/com/panel/GamePanel.java
@@ -0,0 +1,13 @@
+package com.panel;
+
+import com.board.Board;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+	private Board board;
+
+	GamePanel() {
+		setSize(500, 500);
+	}
+}
