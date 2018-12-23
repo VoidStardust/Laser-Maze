48f9ff28343c94cfe70bd714732ad9b4bd1ce63d
Onion12138 <43236962+Onion12138@users.noreply.github.com>
2018-12-23 19:31:31 +0800

    Bug fixed by onion
    add unused chess

--- src/com/board/Rounds.java
+++ src/com/board/Rounds.java
@@ -242,12 +242,16 @@ public class Rounds {
 		board1.addChess(new ReflectorChess(), 2, 2);
 		board1.addChess(new BlockChess(), 2, 0);
 		board1.addChess(new ReceiveChess(), 0, 0);
+		board1.addUnusedChess(new ReceiveChess());
+		board1.addUnusedChess(new ReceiveChess());
 		rounds.add(board1);
 
 		Board board2 = new Board(1, 1);//10
 		board2.addChess(new EmitChess(), 4, 1);
 		board2.addChess(new ReceiveChess(), 0, 4);
 		board2.addChess(new ReceiveChess(), 4, 0);
+		board2.addUnusedChess(new ReceiveChess());
+		board2.addUnusedChess(new ReceiveChess());
 		rounds.add(board2);
 
 		Board board3 = new Board(1, 2);//8
@@ -255,11 +259,15 @@ public class Rounds {
 		board3.addChess(new BlockChess(), 4, 4);
 		board3.addChess(new ChannelChess(Mode.VERTICAL), 0, 2);
 		board3.addChess(new ReceiveChess(), 0, 0);
+		board3.addUnusedChess(new ReceiveChess());
+		board3.addUnusedChess(new ReceiveChess());
 		rounds.add(board3);
 
 		Board board4 = new Board(1, 3);//6
 		board4.addChess(new ReceiveChess(), 0, 1);
 		board4.addChess(new ChannelChess(Mode.VERTICAL), 3, 3);
+		board4.addUnusedChess(new EmitChess());
+		board4.addUnusedChess(new ReflectorChess());
 		rounds.add(board4);
 
 		Board board5 = new Board(1, 4);//11
@@ -267,6 +275,7 @@ public class Rounds {
 		board5.addChess(new ReceiveChess(), 0, 3);
 		board5.addChess(new ReceiveChess(), 2, 0);
 		board5.addChess(new ReceiveChess(), 4, 3);
+		board5.addUnusedChess(new DualReflectorChess());
 		rounds.add(board5);
 
 		Board board6 = new Board(2, 5);//28
@@ -275,6 +284,10 @@ public class Rounds {
 		board6.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 2, 1);
 		board6.addChess(new BlockChess(), 1, 3);
 		board6.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 3, 3);
+		board6.addUnusedChess(new ReceiveChess());
+		board6.addUnusedChess(new ReceiveChess());
+		board6.addUnusedChess(new ReceiveChess());
+		board6.addUnusedChess(new ChannelChess());
 		rounds.add(board6);
 
 		Board board7 = new Board(2, 6);//18
@@ -284,24 +297,34 @@ public class Rounds {
 		board7.addChess(new ReceiveChess(), 3, 1);
 		board7.addChess(new ReceiveChess(), 4, 1);
 		board7.addChess(new ReceiveChess(), 3, 3);
+		board7.addUnusedChess(new EmitChess());
 		rounds.add(board7);
 
 		Board board8 = new Board(2, 7);//17
 		board8.addChess(new EmitChess(), 0, 0);
 		board8.addChess(new ReceiveChess(), 0, 4);
 		board8.addChess(new ReflectorChess(), 1, 3);
+		board8.addUnusedChess(new ReceiveChess());
+		board8.addUnusedChess(new DualReflectorChess());
 		rounds.add(board8);
 
 		Board board9 = new Board(2, 8);//16
 		board9.addChess(new EmitChess(Mode.DOWN), 3, 1);
 		board9.addChess(new ReceiveChess(), 2, 3);
 		board9.addChess(new ReceiveChess(Mode.LEFTUP), 4, 2);
+		board9.addUnusedChess(new ReceiveChess());
+		board9.addUnusedChess(new ReceiveChess());
+		board9.addUnusedChess(new ReceiveChess());
+		board9.addUnusedChess(new DualReflectorChess());
 		rounds.add(board9);
 
 		Board board10 = new Board(2, 9);//19
 		board10.addChess(new EmitChess(), 1, 1);
 		board10.addChess(new ReceiveChess(), 0, 3);
 		board10.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 3, 3);
+		board10.addUnusedChess(new ReceiveChess());
+		board10.addUnusedChess(new ReceiveChess());
+		board10.addUnusedChess(new DualReflectorChess());
 		rounds.add(board10);
 
 		Board board11 = new Board(3, 10);//44
@@ -310,6 +333,9 @@ public class Rounds {
 		board11.addChess(new ReceiveChess(), 4, 1);
 		board11.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 0, 1);
 		board11.addChess(new ReceiveChess(), 0, 3);
+		board11.addUnusedChess(new ReceiveChess());
+		board11.addUnusedChess(new ReceiveChess());
+		board11.addUnusedChess(new EmitChess());
 		rounds.add(board11);
 
 		Board board12 = new Board(3, 11);//31
@@ -319,6 +345,8 @@ public class Rounds {
 		board12.addChess(new ReceiveChess(), 4, 3);
 		board12.addChess(new ReceiveChess(), 0, 0);
 		board12.addChess(new ReceiveChess(), 0, 3);
+		board12.addUnusedChess(new ReceiveChess());
+		board12.addUnusedChess(new ReflectorChess());
 		rounds.add(board12);
 
 		Board board13 = new Board(3, 12);//45 must be receiver
@@ -328,6 +356,10 @@ public class Rounds {
 		board13.addChess(new BlockChess(), 3, 3);
 		board13.addChess(new ReceiveChess(), 4, 2);
 		board13.addChess(new ReceiveChess(), 4, 3);
+		board13.addUnusedChess(new EmitChess());
+		board13.addUnusedChess(new DualReflectorChess());
+		board13.addUnusedChess(new DualReflectorChess());
+		board13.addUnusedChess(new ChannelChess());
 		rounds.add(board13);
 
 		Board board14 = new Board(3, 13);//42
@@ -338,6 +370,9 @@ public class Rounds {
 		board14.addChess(new ReceiveChess(), 4, 4);
 		board14.addChess(new ChannelChess(Mode.HORIZONTAL), 2, 1);
 		board14.addChess(new ReceiveChess(), 0, 4);
+		board14.addUnusedChess(new EmitChess());
+		board14.addUnusedChess(new ReflectorChess());
+		board14.addUnusedChess(new DualReflectorChess());
 		rounds.add(board14);
 
 		Board board15 = new Board(3, 14);//43
@@ -345,6 +380,11 @@ public class Rounds {
 		board15.addChess(new ReflectorChess(Mode.RIGHT), 1, 3);
 		board15.addChess(new ReceiveChess(Mode.LEFTUP), 3, 1);
 		board15.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 4, 1);
+		board15.addUnusedChess(new ReceiveChess());
+		board15.addUnusedChess(new ReceiveChess());
+		board15.addUnusedChess(new ReceiveChess());
+		board15.addUnusedChess(new DualReflectorChess());
+		board15.addUnusedChess(new DualReflectorChess());
 		rounds.add(board15);
 
 		Board board16 = new Board(4, 15);//60
@@ -354,6 +394,11 @@ public class Rounds {
 		board16.addChess(new ChannelChess(), 2, 2);
 		board16.addChess(new ReceiveChess(Mode.LEFTUP), 3, 4);
 		board16.addChess(new ReflectorChess(), 4, 1);
+		board16.addUnusedChess(new ReceiveChess());
+		board16.addUnusedChess(new ReceiveChess());
+		board16.addUnusedChess(new DualReflectorChess());
+		board16.addUnusedChess(new DualReflectorChess());
+		board16.addUnusedChess(new EmitChess());
 		rounds.add(board16);
 
 		Board board17 = new Board(4, 16);//59
@@ -364,6 +409,10 @@ public class Rounds {
 		board17.addChess(new ReflectorChess(), 2, 2);
 		board17.addChess(new ReceiveChess(), 1, 0);
 		board17.addChess(new ReceiveChess(), 2, 0);
+		board17.addUnusedChess(new ReceiveChess());
+		board17.addUnusedChess(new ReceiveChess());
+		board17.addUnusedChess(new ReceiveChess());
+		board17.addUnusedChess(new DualReflectorChess());
 		rounds.add(board17);
 
 		Board board18 = new Board(4, 17);//58
@@ -373,6 +422,9 @@ public class Rounds {
 		board18.addChess(new ReceiveChess(Mode.LEFTDOWN), 2, 0);
 		board18.addChess(new ReceiveChess(), 3, 0);
 		board18.addChess(new ReceiveChess(Mode.LEFTDOWN), 4, 0);
+		board18.addUnusedChess(new ReceiveChess());
+		board18.addUnusedChess(new ReceiveChess());
+		board18.addUnusedChess(new DualReflectorChess());
 		rounds.add(board18);
 
 		Board board19 = new Board(4, 18);//57
@@ -383,13 +435,21 @@ public class Rounds {
 		board19.addChess(new ChannelChess(), 1, 1);
 		board19.addChess(new ReceiveChess(Mode.RIGHTRIGHT), 2, 1);
 		board19.addChess(new ReceiveChess(Mode.RIGHTLEFT), 0, 2);
+		board19.addUnusedChess(new ReceiveChess());
+		board19.addUnusedChess(new ReceiveChess());
+		board19.addUnusedChess(new DualReflectorChess());
+		board19.addUnusedChess(new DualReflectorChess());
 		rounds.add(board19);
 
-		Board board20 = new Board(4, 19);
+		Board board20 = new Board(4, 19);//49
 		board20.addChess(new EmitChess(Mode.LEFT), 2, 4);
 		board20.addChess(new DualReflectorChess(), 1, 1);
 		board20.addChess(new DualReflectorChess(), 3, 2);
 		board20.addChess(new ReceiveChess(), 1, 4);
+		board20.addUnusedChess(new ReceiveChess());
+		board20.addUnusedChess(new ReceiveChess());
+		board20.addUnusedChess(new ReceiveChess());
+		board20.addUnusedChess(new ReceiveChess());
 		rounds.add(board20);
 	}
 }
